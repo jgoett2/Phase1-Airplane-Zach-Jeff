f10817099fbe7e9d2b47c30077b16624ec9826b1
Jeffrey Goett <jgoett2@yahoo.com>
2024-09-13 13:27:57 -0400

    add source code files.

---
+++ scr/data_visualizations.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def hi(df, word, stat, ax):
+    dfB = df[df['Make'] == word]
+    GB = dfB.groupby('Make_Model').count().sort_values(by='Event.Id', ascending=False)
+    top_five = GB.index[:5]
+    top_five
+    ave_fraction = dfB[["Make_Model",stat]].groupby("Make_Model").mean()
+    ave_fraction
+    
+    x = pd.DataFrame([(x, float(ave_fraction.loc[x])) for x in top_five]).sort_values(by=1)
+
+    sns.barplot(data=x, x=0, y = 1, ax=ax)
+    ax.set_xlabel('Make_Model')
+    ax.set_ylabel(stat)
+    ax.set_title(word+' Models')
+
+
+
+def performance_make_damage(df, stat):
+  fig, ax = plt.subplots(1,2,figsize=[14,5])
+  df_substantial = df[df["Aircraft.damage"] == "Substantial"]
+  df_destroyed = df[df["Aircraft.damage"] == "Destroyed"]
+
+  order = df_substantial[["Make",stat]].groupby("Make").mean().sort_values(by=stat, ascending=False).index
+  sns.barplot(data=df_substantial, x='Make', y = stat, order=order, ax=ax[0])
+  ax[0].set_title(stat + " vs. Make for Substantial Damage")
+
+  order = df_destroyed[["Make",stat]].groupby("Make").mean().sort_values(by=stat, ascending=False).index
+  sns.barplot(data=df_destroyed, x='Make', y = stat, order=order, ax=ax[1])
+  ax[1].set_title(stat + " vs. Make for Destroyed Damage")
+
+  fig.savefig("Images/" + stat + "Make")
+
+
+def performance_phase(df, stat):
+  fig, ax = plt.subplots(figsize= (12,6))
+
+  phases_flight = df["Broad.phase.of.flight"].value_counts().index[:-2]
+  df_phases = df[df["Broad.phase.of.flight"].map(lambda x: x in phases_flight)]
+
+  order = df[["Broad.phase.of.flight",stat]].groupby("Broad.phase.of.flight").mean().sort_values(by=stat,ascending=False).index
+
+  sns.barplot(data=df, y='Broad.phase.of.flight', x = stat, order=order, ax=ax)
+  ax.set_title(stat + " vs. Phase of Flight", fontsize=16, fontweight = 'bold')
+
+  ax.tick_params(labelsize=12)
+  ax.set_xlabel(stat, fontweight = 'bold', fontsize=14)
+  ax.set_ylabel('Phase of flight', fontweight = 'bold', fontsize=14)
+
+  fig.savefig("Images/PhaseOfFlight" + stat)
+
+
+
+def best_models(df):
+   dfPC = df[(df['Make'] == 'Cessna') | (df['Make'] == 'Piper') ]
+   dfPC = dfPC[dfPC["Aircraft.damage"] == "Destroyed"]
+   dfPCd = dfPC.groupby("Make_Model").count().sort_values(by="Event.Id",ascending=False)
+   top_5 = dfPCd.index[:5]
+   ave_frac = dfPC[["Make_Model","Fraction_uninjured"]].groupby("Make_Model").mean()
+   xy = pd.DataFrame([(x,float(ave_frac.loc[x])) for x in top_5]).sort_values(by=1)
+   fig, ax = plt.subplots(figsize = (10,7))
+
+   sns.barplot(data=xy, x=0, y=1)
+   ax.set_xlabel("Make_Model")
+   ax.set_ylabel("Fraction Uninjured")
+   ax.set_title("Destroyed per Make_Model in Uninjured")
+
+   fig.savefig("Images/Make_ModelDestroyedUninjured")
